08cbcd6420037cd6473f8383a1cf4dbeb9883528
Ardika Bagus S <me@ardikabs.com>
2021-09-20 01:14:51 +0700

    feat: add unittest (#6)
    + also upgrade go to 1.16

--- internal/aws/aws.go
+++ internal/aws/aws.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
-	aws_session "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
 	"awssh/internal/logging"
@@ -17,60 +17,26 @@ import (
 // or loaded from AWS shared-credentials located in ~/.aws/credentials
 // in particularly when you need to use AWS_PROFILE located in ~/.aws/config
 // you need to set AWS_SDK_LOAD_CONFIG=1
-func NewSession(region string) (session *aws_session.Session) {
-	session = aws_session.Must(aws_session.NewSession(&aws.Config{
+func NewSession(region string) *session.Session {
+	session := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	}))
 
 	logging.Logger().Debugf("Region: %s", *session.Config.Region)
 
-	return
+	return session
 }
 
-// GetInstanceWithID used to get instance with InstanceID input
-func GetInstanceWithID(session *aws_session.Session, instanceID string) (ec2Instances []*EC2Instance, err error) {
-	input := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{
-			aws.String(instanceID),
-		},
-	}
-
-	logging.Logger().Debugf("Filter EC2 instances with InstanceID: %s", instanceID)
-	ec2Instances, err = getInstance(session, input)
-
-	return
-}
-
-// GetInstanceWithTag used to get instance with key-value pair tags input (ex: Environment=production,ProductDomain=VirtualProduct)
-func GetInstanceWithTag(session *aws_session.Session, tags string) (ec2Instances []*EC2Instance, err error) {
-	filters, err := prepareFilters(tags)
-
-	if err != nil {
-		return
-	}
-
-	input := &ec2.DescribeInstancesInput{
-		Filters: filters,
-	}
-
-	logging.Logger().Debugf("Filter EC2 instances with tags: %s", tags)
-	ec2Instances, err = getInstance(session, input)
-
-	return
-}
-
-// prepareFilters used to form a proper AWS filter tags format
-// from a raw tags input format
-func prepareFilters(rawTags string) (filters []*ec2.Filter, err error) {
+func PrepareEC2Filters(tags string) ([]*ec2.Filter, error) {
 	awsTags := make(map[string][]*string)
 
-	splitTags := strings.Split(rawTags, ",")
+	splitTags := strings.Split(tags, ",")
 
 	for _, tags := range splitTags {
 		part := strings.Split(tags, "=")
 
 		if len(part) != 2 {
-			return nil, fmt.Errorf("Wrong tag format, it should be follow 'Key=Value' format: '%s'", tags)
+			return nil, fmt.Errorf("awssh: bad input, filters must be using 'Key=Value' format: '%s'", tags)
 		}
 
 		key := part[0]
@@ -78,6 +44,7 @@ func prepareFilters(rawTags string) (filters []*ec2.Filter, err error) {
 		awsTags[key] = append(awsTags[key], value)
 	}
 
+	filters := make([]*ec2.Filter, 0)
 	filters = append(filters, &ec2.Filter{
 		Name: aws.String("instance-state-name"),
 		Values: []*string{
@@ -93,44 +60,15 @@ func prepareFilters(rawTags string) (filters []*ec2.Filter, err error) {
 		filters = append(filters, f)
 	}
 
-	logging.Logger().Debugf("Use the following filters to filter EC2 instances: %v", filters)
-
-	return
+	return filters, nil
 }
 
-// getInstance handle the underlaying to gather the EC2 instances
-// following with the DescribeInstances method
-func getInstance(session *aws_session.Session, input *ec2.DescribeInstancesInput) (ec2Instances []*EC2Instance, err error) {
-	svc := ec2.New(session)
-	result, err := svc.DescribeInstances(input)
-
-	if err != nil {
-		return nil, fmt.Errorf("Failed to get instance: (%v)", err)
-	}
-
-	if len(result.Reservations) == 0 {
-		return nil, fmt.Errorf("No instance is found on region %s", *session.Config.Region)
-	}
-
-	reservations := result.Reservations
-
-	for i := range reservations {
-		for _, instance := range reservations[i].Instances {
-			ec2 := NewEC2Instance(session, instance)
-			ec2Instances = append(ec2Instances, ec2)
-		}
-	}
-
-	logging.Logger().Debugf("Found %d EC2 instances on region %s", len(ec2Instances), *session.Config.Region)
-
-	return
-}
-
-func getTagValue(key string, instance *ec2.Instance) string {
+func GetTagValue(key string, instance *ec2.Instance) string {
 	for _, tag := range instance.Tags {
 		if *tag.Key == key {
 			return *tag.Value
 		}
 	}
+
 	return ""
 }
---
+++ internal/aws/aws_test.go
@@ -0,0 +1,134 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrepareEC2Filters(t *testing.T) {
+	type args struct {
+		tags string
+	}
+
+	defaultFilters := []*ec2.Filter{
+		{
+			Name: aws.String("instance-state-name"),
+			Values: []*string{
+				aws.String("running"),
+			},
+		},
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		expected []*ec2.Filter
+		err      error
+	}{
+		{
+			name:     "no argument (empty)",
+			args:     args{""},
+			expected: nil,
+			err:      fmt.Errorf("awssh: bad input, filters must be using 'Key=Value' format: ''"),
+		},
+		{
+			name:     "wrong format argument",
+			args:     args{"Environment,production"},
+			expected: nil,
+			err:      fmt.Errorf("awssh: bad input, filters must be using 'Key=Value' format: 'Environment'"),
+		},
+		{
+			name: "single tag",
+			args: args{"Environment=production"},
+			expected: append(defaultFilters,
+				&ec2.Filter{
+					Name: aws.String("tag:Environment"),
+					Values: []*string{
+						aws.String("production"),
+					},
+				},
+			),
+			err: nil,
+		},
+		{
+			name: "multiple tags with comma delimiters",
+			args: args{"Environment=production,Service=promotion"},
+			expected: append(defaultFilters,
+				&ec2.Filter{
+					Name: aws.String("tag:Environment"),
+					Values: []*string{
+						aws.String("production"),
+					},
+				},
+				&ec2.Filter{
+					Name: aws.String("tag:Service"),
+					Values: []*string{
+						aws.String("promotion"),
+					},
+				},
+			),
+			err: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want, err := PrepareEC2Filters(tt.args.tags)
+			if tt.err != nil {
+				assert.NotNil(t, err)
+				return
+			}
+
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, want)
+
+		})
+	}
+}
+
+func TestGetTagValue(t *testing.T) {
+	defaultInstance := &ec2.Instance{
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Environment"),
+				Value: aws.String("production"),
+			},
+		},
+	}
+
+	type args struct {
+		key      string
+		instance *ec2.Instance
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		expected string
+		err      error
+	}{
+		{
+			name:     "instance with no specified tags",
+			args:     args{"ManagedBy", defaultInstance},
+			expected: "",
+			err:      nil,
+		},
+		{
+			name:     "instance with specified tags",
+			args:     args{"Environment", defaultInstance},
+			expected: "production",
+			err:      nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := GetTagValue(tt.args.key, tt.args.instance)
+			assert.Equal(t, tt.expected, want)
+		})
+	}
+}
